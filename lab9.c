5e873da38d2e1608493536c958adfbd980bdfb8b
Tyler <tyler.tran88@yahoo.com>
2024-04-11 21:20:58 -0400

    added lab9

--- lab9.c
+++ lab9.c
@@ -1,75 +1,83 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // Added for malloc and exit
+
 // RecordType
 struct RecordType
 {
-	int		id;
-	char	name;
-	int		order; 
+    int     id;
+    char    name;
+    int     order; 
+};
+
+
+struct Node {
+    struct RecordType data;
+    struct Node* next;
 };
 
 // Fill out this structure
 struct HashType
-{	
-	int id;
-	char name;
-	int order;
-
+{
+    struct Node** array; 
+    int size; 
 };
 
-// Compute the hash function
-int hash(int x)
-{	return x % 10;
 
+int hash(int x, int size)
+{
+    return x % size; 
 }
 
 // parses input file to an integer array
 int parseData(char* inputFileName, struct RecordType** ppData)
 {
-	FILE* inFile = fopen(inputFileName, "r");
-	int dataSz = 0;
-	int i, n;
-	char c;
-	struct RecordType *pRecord;
-	*ppData = NULL;
+    FILE* inFile = fopen(inputFileName, "r");
+    int dataSz = 0;
+    int i, n;
+    char c;
+    struct RecordType *pRecord;
+    *ppData = NULL;
+if (inFile == NULL) {
+    printf("Error opening file: %s\n", inputFileName);
+    return -1;
+}
+    if (inFile)
+    {
+        fscanf(inFile, "%d\n", &dataSz);
+        *ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
+        
+        if (*ppData == NULL)
+        {
+            printf("Cannot allocate memory\n");
+            exit(-1);
+        }
+        for (i = 0; i < dataSz; ++i)
+        {
+            pRecord = *ppData + i;
+            fscanf(inFile, "%d ", &n);
+            pRecord->id = n;
+            fscanf(inFile, "%c ", &c);
+            pRecord->name = c;
+            fscanf(inFile, "%d ", &n);
+            pRecord->order = n;
+        }
 
-	if (inFile)
-	{
-		fscanf(inFile, "%d\n", &dataSz);
-		*ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
-		// Implement parse data block
-		if (*ppData == NULL)
-		{
-			printf("Cannot allocate memory\n");
-			exit(-1);
-		}
-		for (i = 0; i < dataSz; ++i)
-		{
-			pRecord = *ppData + i;
-			fscanf(inFile, "%d ", &n);
-			pRecord->id = n;
-			fscanf(inFile, "%c ", &c);
-			pRecord->name = c;
-			fscanf(inFile, "%d ", &n);
-			pRecord->order = n;
-		}
+        fclose(inFile);
+    }
 
-		fclose(inFile);
-	}
-
-	return dataSz;
+    return dataSz;
 }
 
 // prints the records
 void printRecords(struct RecordType pData[], int dataSz)
 {
-	int i;
-	printf("\nRecords:\n");
-	for (i = 0; i < dataSz; ++i)
-	{
-		printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
-	}
-	printf("\n\n");
+    int i;
+    printf("\nRecords:\n");
+    for (i = 0; i < dataSz; ++i)
+    {
+        printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
+    }
+    printf("\n\n");
 }
 
 // display records in the hash structure
@@ -78,49 +86,75 @@ void printRecords(struct RecordType pData[], int dataSz)
 // index x -> id, name, order -> id, name, order ....
 void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
 {
-	int i;
+    int i;
 
-	for (i=0;i<hashSz;++i)
-	{	
-		printf("Index %d ->", i);
-		struct HashType* current = pHashArray[i].order;
-		while(current != NULL){
-		printf("%d %c %d", pHashArray->id, pHashArray->name, pHashArray->order);
-		
-		}
-	}
+    for (i = 0; i < hashSz; ++i)
+    {
+        printf("Index %d -> ", i);
+        struct Node* current = pHashArray->array[i];
+        while (current != NULL) {
+            printf("%d %c %d -> ", current->data.id, current->data.name, current->data.order);
+            current = current->next;
+        }
+        printf("\n");
+    }
 }
 
 int main(void)
-{	
-	struct RecordType *pRecords;
-	struct HashType *pHash;
-	int recordSz = 0;
-	int hashSz = 0;
-	recordSz = parseData("input_lab_9.txt", &pRecords);
-	printRecords(pRecords, recordSz);
-	
-	for(int i = 0; i < hashSz; i++){
-		pHash[i].order = NULL;
-	}
-	for(int i = 0; i <recordSz; i++){
-		int index = hash(pRecords[i].id);
-		struct HashType* newNode = (struct HashType*)malloc(sizeof(struct HashType));
-        if(newNode ==NULL){
-			printf("Memory allocation failed.\n");
-		}
-		newNode->id = pRecords[i]; 
-        newNode->order = pHash[index].order; 
-        pHash[index].order = newNode;
-	}
-printf("Records in the hash table:\n");
-    for (int i = 0; i < hashSz; ++i) {
-        printf("Index %d -> ", i);
-        struct HashType* current = pHash[i].order;
-        while (current != NULL) {
-            printf("%d %c %d -> ", current->id, current->name, current->order);
-            current = current->order;
-        }
+{
+    struct RecordType *pRecords;
+    int recordSz = 0;
 
-}
+    recordSz = parseData("input_lab_9.txt", &pRecords);
+    printRecords(pRecords, recordSz);
+	printf("Number of records parsed: %d\n", recordSz);
+    
+    int hashSize = recordSz * 2; 
+    struct HashType hashTable;
+    hashTable.size = hashSize;
+    hashTable.array = (struct Node**)malloc(hashSize * sizeof(struct Node*));
+    if (hashTable.array == NULL) {
+        printf("Memory allocation failed.\n");
+        return -1;
+    }
+    for (int i = 0; i < hashSize; ++i) {
+        hashTable.array[i] = NULL;
+    }
+
+    
+    for (int i = 0; i < recordSz; ++i) {
+        int index = hash(pRecords[i].id, hashSize);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        if (newNode == NULL) {
+            printf("Memory allocation failed.\n");
+            return -1;
+        }
+        newNode->data = pRecords[i];
+        newNode->next = NULL;
+        if (hashTable.array[index] == NULL) {
+            hashTable.array[index] = newNode;
+        } else {
+            
+            newNode->next = hashTable.array[index];
+            hashTable.array[index] = newNode;
+        }
+		printf("Index: %d\n", index);
+    }
+
+    
+    displayRecordsInHash(&hashTable, hashSize);
+
+
+    for (int i = 0; i < hashSize; ++i) {
+        struct Node* current = hashTable.array[i];
+        while (current != NULL) {
+            struct Node* temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(hashTable.array);
+    free(pRecords);
+
+    return 0;
 }
